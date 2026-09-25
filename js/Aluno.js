6fa96d5e0f74da9444a504ed7ff9935bb7f3ce9a
Suyanne Cristina <suyanne.dev@gmail.com>
2026-09-25 12:11:09 -0300

    implementa classe Aluno

---
+++ js/Aluno.js
@@ -0,0 +1,17 @@
+export class Aluno {
+ constructor(nome, genero, dataNascimento, cpf, telefone, email, cep, cidade, estado, logradouro, numero, complemento, bairro) {
+  this.nome = nome;
+  this.genero = genero;
+  this.dataNascimento = dataNascimento;
+  this.cpf = cpf;
+  this.telefone = telefone;
+  this.email = email;
+  this.cep = cep;
+  this.cidade = cidade;
+  this.estado = estado;
+  this.logradouro = logradouro;
+  this.numero = numero;
+  this.complemento = complemento;
+  this.bairro = bairro;
+ }
+}
